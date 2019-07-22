48d481c0e6e0981aff38191b4090949de6b63cae
scbushan05 <sc.bushan.05@gmail.com>
2019-07-22 23:15:33 +0530

    Added the sql file

---
+++ queries.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE analytics;
+
+USE analytics;
+
+--Line chart 
+CREATE TABLE line_chart
+(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    category VARCHAR(45) NOT NULL,
+    series DOUBLE NOT NULL
+);
+
+INSERT INTO line_chart(category, series) VALUES('Jan', 29.9);
+INSERT INTO line_chart(category, series) VALUES('Feb', 71.5);
+INSERT INTO line_chart(category, series) VALUES('Mar', 106.4);
+INSERT INTO line_chart(category, series) VALUES('Apr', 129.2);
+INSERT INTO line_chart(category, series) VALUES('May', 144.0);
+INSERT INTO line_chart(category, series) VALUES('Jun', 176.0);
+INSERT INTO line_chart(category, series) VALUES('Jul', 135.6);
+INSERT INTO line_chart(category, series) VALUES('Aug', 148.5);
+INSERT INTO line_chart(category, series) VALUES('Sep', 216.4);
+INSERT INTO line_chart(category, series) VALUES('Oct', 194.4);
+INSERT INTO line_chart(category, series) VALUES('Nov', 95.6);
+INSERT INTO line_chart(category, series) VALUES('Dec', 54.4);
+
+SELECT * FROM line_chart;
+--End Line chart
